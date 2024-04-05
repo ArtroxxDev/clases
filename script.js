b541cc1877374859d111b6edeb6dc71b6312287e
Matias López <alopez@eglobalt.com.py>
2024-04-05 13:57:57 -0400

    second

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
-    // Ruta al archivo ZIP que deseas que tus usuarios descarguen.
-    // Debes subir este archivo a tu repositorio en GitHub.
+
     const zipFileUrl = './Downloads.zip';
     
-    // Creando un elemento <a> para simular un clic y comenzar la descarga
     const a = document.createElement('a');
     a.href = zipFileUrl;
     a.setAttribute('download', '');
